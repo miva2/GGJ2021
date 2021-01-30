31808a93e5ce0e8cc030252474745eaaf39a2722
miva2 <miva2-_-@hotmail.com>
2021-01-30 12:59:03 +0100

    First Scene and scripts

---
+++ GGJ2021 - Break Key/Assets/Scripts/KeyCollection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct KeyCollection
+{
+    public bool KeyW;
+    public bool KeyA;
+    public bool KeyS;
+    public bool KeyD;
+    public bool KeySpace;
+    public bool KeyQ;
+    public bool KeyE;
+    public bool KeyQuestionMark;
+    public bool KeyEnter;
+    
+    
+
+}
