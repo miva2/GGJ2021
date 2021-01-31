0b73b4da59b23acbe222d49af6656c5848eed3fd
miva2 <miva2-_-@hotmail.com>
2021-01-31 13:23:49 +0100

    Enable Key UI element when picking up new keyboard key

--- GGJ2021 - Break Key/Assets/Scripts/SceneManager.cs
+++ GGJ2021 - Break Key/Assets/Scripts/SceneManager.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
 {
     public Toast toast;
-    
+    public KeyboardKeyUI[] keyCollection; 
+
     private Dictionary<KeyboardKey, KeyboardAction> collectedKeys = new Dictionary<KeyboardKey, KeyboardAction>();
     
     private void Start()
@@ -22,6 +24,14 @@ public class SceneManager : MonoBehaviour
         Debug.Log($"item {item} added to collection!");
         collectedKeys.Add(item.keyboardKey, KeyboardAction.UNDEFINED); // KeyboardActions are not supported yet
         toast.ShowToast($"KEY {item.keyboardKey} picked up!");
+        foreach (KeyboardKeyUI key in keyCollection)
+        {
+            if (key.key.Equals(item.keyboardKey))
+            {
+                Debug.Log($"enabling the UI for {item.keyboardKey}, UI element: {key}");
+                key.gameObject.GetComponent<Image>().enabled = true;
+            }
+        } // This can probably shorter and cleaner but don't know it in C#. Check it later :D 
     }
     
     public bool IsKeyObtained(KeyboardKey key)
