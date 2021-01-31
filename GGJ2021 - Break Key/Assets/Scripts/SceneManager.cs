66337c241645da06597005503840c1e2e658b328
miva2 <miva2-_-@hotmail.com>
2021-01-31 15:53:31 +0100

    start animation

--- GGJ2021 - Break Key/Assets/Scripts/SceneManager.cs
+++ GGJ2021 - Break Key/Assets/Scripts/SceneManager.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
 {
+    public GameObject player;
+    public Camera camera;
     public Toast toast;
-    public KeyboardKeyUI[] keyCollection; 
+    public KeyboardKeyUI[] keyCollection;
 
+    private bool startAnimationPerformed = false;
     private Dictionary<KeyboardKey, KeyboardAction> collectedKeys = new Dictionary<KeyboardKey, KeyboardAction>();
     
     private void Start()
@@ -17,8 +21,28 @@ public class SceneManager : MonoBehaviour
         // collectedKeys.Add(KeyboardKey.S, KeyboardAction.UNDEFINED);
         collectedKeys.Add(KeyboardKey.D, KeyboardAction.UNDEFINED);
 
+        // StartCoroutine(LaunchKeyboard()); // looks stupid
+
     }
-    
+
+    private void Update()
+    {
+        if (!startAnimationPerformed)
+        {
+            player.transform.Translate(-4.5f, 0.5f, 0f);
+            startAnimationPerformed = true;
+        }
+    }
+
+    // looks stupid using the coroutine, just using earlier method for now
+    // private IEnumerator LaunchKeyboard()
+    // {
+    //     yield return new WaitForSeconds(1f); //hardcoded wait, should wait until scene is loaded instead
+    //     player.transform.Translate(-4.8f, 0.5f, 0f);
+    //     //hardcoded camera but whatever
+    //     // camera should start a bit higher and follow keyboard until on the floor and then follow normal code. no more time.
+    // }
+
     public void KeyboardKeyPickedUp(KeyboardKeyItem item)
     {
         Debug.Log($"item {item} added to collection!");
